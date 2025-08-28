4a604f6a4d5b5a3546695e1036ecac6b42c8b0b5
Dyathos <147958904+Dyathos@users.noreply.github.com>
2025-08-28 02:07:50 +0100

    fix: allow strapi.orientation-maroc.com in allowedHosts

--- config/server.js
+++ config/server.js
@@ -4,6 +4,8 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('PUBLIC_URL', 'https://strapi.orientation-maroc.com'),
+  allowedHosts: env.array('ALLOWED_HOSTS', ['strapi.orientation-maroc.com']),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
